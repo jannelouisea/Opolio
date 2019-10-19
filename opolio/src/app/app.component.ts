3569f0631dc6580b72b504c8b04dc721afc5847c
jannelouisea <janne.louise.a@gmail.com>
2019-10-19 19:05:51 -0400

    Initial commit of angular app with background styles

---
+++ opolio/src/app/app.component.ts
@@ -0,0 +1,12 @@
+//import '../styles/main';
+import { Component } from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent {
+  //title = 'opolio';
+}
